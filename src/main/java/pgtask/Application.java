edc70d895c255285f7b63022d07673b1e322e911
Cesar Castillo <ccastill100@gmail.com>
2023-02-09 10:08:20 +0100

    TEMP: Make the fetched number of question visible at boot time

--- src/main/java/pgtask/Application.java
+++ src/main/java/pgtask/Application.java
@@ -1,10 +1,24 @@
 package pgtask;
 
+import ccb.pgames.QuestionLoader;
+import io.micronaut.context.ApplicationContext;
 import io.micronaut.runtime.Micronaut;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.util.Optional;
 
 public class Application {
 
+
+    @Inject
+    @Named("fetcher")
+    private QuestionLoader questionLoader;
+
     public static void main(String[] args) {
-        Micronaut.build(args).eagerInitSingletons(true).mainClass(Application.class).args(args).start();
+        ApplicationContext applicationContext = Micronaut.build(args).eagerInitSingletons(true).mainClass(Application.class).args(args).start();
+        Optional<QuestionLoader> fetcher = applicationContext.findBean(QuestionLoader.class);
+        System.out.println("Fetched questions: " + fetcher.get().getSize());
+
     }
 }
