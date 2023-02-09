fa8526ca302a6b0343d981b17120a96497eb95b5
Cesar Castillo <ccastill100@gmail.com>
2023-02-09 10:07:54 +0100

    Usage fetcher client to request latest 20 questions

--- src/main/java/ccb/pgames/QuestionLoader.java
+++ src/main/java/ccb/pgames/QuestionLoader.java
@@ -1,18 +1,22 @@
 package ccb.pgames;
 
+import ccb.pgames.backends.StackOverFlowClient;
+import ccb.pgames.model.Question;
+import jakarta.inject.Named;
 import jakarta.inject.Singleton;
 
-import java.util.ArrayList;
 import java.util.List;
 
+@Named("fetcher")
 @Singleton
 public class QuestionLoader {
+    List<Question> dbQuestions;
 
-    List<String> dbQuestions;
+    public QuestionLoader(StackOverFlowClient fetcher) {
+        dbQuestions = fetcher.latestQuestions(1, 20, "creation", "desc", "stackoverflow").getItems();
+    }
 
-    public QuestionLoader() {
-        dbQuestions = new ArrayList<>();
-        dbQuestions.add("Q100");
-        dbQuestions.add("Q200");
+    public int getSize() {
+        return this.dbQuestions.size();
     }
 }
