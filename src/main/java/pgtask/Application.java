549a270f714e487e8747f9f111928298191234c2
Cesar Castillo <ccastill100@gmail.com>
2023-02-09 18:58:42 +0100

    Formatting changes

--- src/main/java/pgtask/Application.java
+++ src/main/java/pgtask/Application.java
@@ -1,12 +1,9 @@
 package pgtask;
 
 import ccb.pgames.QuestionLoader;
-import io.micronaut.context.ApplicationContext;
 import io.micronaut.runtime.Micronaut;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
-
-import java.util.Optional;
 
 public class Application {
 
@@ -16,9 +13,12 @@ public class Application {
     private QuestionLoader questionLoader;
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = Micronaut.build(args).eagerInitSingletons(true).mainClass(Application.class).args(args).start();
-        Optional<QuestionLoader> fetcher = applicationContext.findBean(QuestionLoader.class);
-        System.out.println("Fetched questions: " + fetcher.get().getSize());
-
+        var applicationContext = Micronaut.build(args)
+                .eagerInitSingletons(true)
+                .mainClass(Application.class)
+                .args(args).start();
+        //TODO: get() is tolerated here because we trust in the previous wiring of beans
+        var fetcher = applicationContext.findBean(QuestionLoader.class).get();
+        fetcher.fetch();
     }
 }
