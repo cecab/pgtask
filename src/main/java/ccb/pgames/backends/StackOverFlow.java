c855ffdf1b193c981d568b938e2b8e8c2b2f5ee1
Cesar Castillo <ccastill100@gmail.com>
2023-02-09 10:10:28 +0100

    Add future tasks as todo comments for a centralized config file

--- src/main/java/ccb/pgames/backends/StackOverFlow.java
+++ src/main/java/ccb/pgames/backends/StackOverFlow.java
@@ -5,6 +5,7 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
 
 public interface StackOverFlow {
+    //TODO: Move this value to main config for the App
     @Get("/2.3/questions/featured")
     FeaturedResponse latestQuestions(@QueryValue int page, @QueryValue int pagesize, @QueryValue String sort, @QueryValue("order") String order, @QueryValue("site") String site);
 }
--- src/main/java/ccb/pgames/backends/StackOverFlowClient.java
+++ src/main/java/ccb/pgames/backends/StackOverFlowClient.java
@@ -4,6 +4,7 @@ import ccb.pgames.backends.StackOverFlow;
 import ccb.pgames.model.FeaturedResponse;
 import io.micronaut.http.client.annotation.Client;
 
+//TODO: Move this value into a configuration file for the App
 @Client("https://api.stackexchange.com")
 public interface StackOverFlowClient extends StackOverFlow {
     @Override
