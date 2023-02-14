59a3a096b5e9cf1f28c9fe3732e1e4fc0b037357
Cesar Castillo <ccastill100@gmail.com>
2023-02-14 16:19:32 +0100

    Test parsing of latest question response

--- src/test/java/ccb/pgames/backends/StackOverFlowTest.java
+++ src/test/java/ccb/pgames/backends/StackOverFlowTest.java
@@ -10,27 +10,52 @@ import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.io.ResourceLoader;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.runtime.server.EmbeddedServer;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+//TODO: We do not need Jdbi for this test. The dependency on testContainer must be removed.
+@Testcontainers
 class StackOverFlowTest {
+    @Container
+    public static GenericContainer POSTGRESQL_CONTAINER;
+    static final EmbeddedServer embeddedServer;
+
+    private static Map<String, Object> buildDataSourceSettings(GenericContainer POSTGRESQL_CONTAINER) {
+        String host = POSTGRESQL_CONTAINER.getHost();
+        Integer mappedPort = POSTGRESQL_CONTAINER.getFirstMappedPort();
+        return Map.of("datasources.default.url", "jdbc:postgresql://" + host + ":" + mappedPort + "/postgres",
+                "datasources.default.username", "pgtask", "datasources.default.password", "pgtask-password");
+    }
+
+    static {
+        POSTGRESQL_CONTAINER = new GenericContainer<>("postgres").withExposedPorts(5432).withEnv(Map.of(
+                "POSTGRES_USER", "pgtask", "POSTGRES_PASSWORD", "pgtask-password"));
+        POSTGRESQL_CONTAINER.start();
+
+        embeddedServer = ApplicationContext.run(EmbeddedServer.class, buildDataSourceSettings(POSTGRESQL_CONTAINER));
+    }
 
     @Test
     void verifyQuestionsCanBeFetchedTest() {
-        EmbeddedServer pgTask = ApplicationContext.run(EmbeddedServer.class);
-        EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer.class,
-                Map.of("micronaut.http.services" + ".stackexchange.url", "http://localhost:" + pgTask.getPort(),
-                        "spec.name", "checkTest"
-                )
-        );
-        var httpClient = embeddedServer.getApplicationContext().createBean(StackExchangeMockServer.class, embeddedServer.getURL());
+        Map<String, Object> settings = new HashMap<>();
+        settings.put("micronaut.http.services" + ".stackexchange.url", "http://localhost:" + 18700); //TODO: make
+        // 18700 a dynamic free port available discovered
+        settings.put("spec.name", "checkTest");
+        settings.putAll(buildDataSourceSettings(POSTGRESQL_CONTAINER));
+        EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer.class, settings);
+        var httpClient = embeddedServer.getApplicationContext().createBean(StackExchangeMockServer.class,
+                embeddedServer.getURL());
         var rsp = httpClient.latestQuestions(1, 20, "desc", "creation", "stackoverflow");
         assertEquals(5, rsp.getItems().size());
     }
@@ -56,7 +81,6 @@ class StackOverFlowTest {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-
         }
 
         @Override
