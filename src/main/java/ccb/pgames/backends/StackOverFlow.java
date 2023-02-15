90eb13fa35c86f2decbac0e71d11459e25d20618
Cesar Castillo <ccastill100@gmail.com>
2023-02-15 10:53:02 +0100

    Prepare condition to not start bean that fetch questions

--- src/main/java/ccb/pgames/backends/StackOverFlow.java
+++ src/main/java/ccb/pgames/backends/StackOverFlow.java
@@ -2,12 +2,14 @@ package ccb.pgames.backends;
 
 import ccb.pgames.backends.models.Question;
 import ccb.pgames.backends.models.User;
+import io.micronaut.context.annotation.Requires;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.http.client.annotation.Client;
 
-@Client(id="stackexchange")
+@Client(id = "stackexchange")
+@Requires(missingProperty = "spec.name")
 public interface StackOverFlow {
     @Get("/questions/featured")
     StackResponse<Question> latestQuestions(@QueryValue int page, @QueryValue int pagesize, @QueryValue String sort,
